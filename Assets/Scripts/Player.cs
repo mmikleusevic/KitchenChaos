84c56768c3b5a26b94e2765ee383081bdde65767
mikleusevicmarin <mikleusevicmarin@gmail.com>
2023-09-14 13:51:34 +0200

    Added player, animations, walking

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking = false;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -31,9 +32,16 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
         transform.position += moveDir * Time.deltaTime * moveSpeed;
 
-        Debug.Log(inputVector);
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
