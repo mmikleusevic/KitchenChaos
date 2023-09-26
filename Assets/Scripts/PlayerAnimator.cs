28c846fe0459d39e1cfe58920684b6d8f5978ae9
mikleusevicmarin <mikleusevicmarin@gmail.com>
2023-09-26 13:13:05 +0200

    synced player animations

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,6 +1,7 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class PlayerAnimator : NetworkBehaviour
 {
     private const string IS_WALKING = "IsWalking";
 
@@ -15,6 +16,8 @@ public class PlayerAnimator : MonoBehaviour
 
     private void Update()
     {
+        if (!IsOwner) return;
+
         animator.SetBool(IS_WALKING, player.IsWalking());
     }
 }
