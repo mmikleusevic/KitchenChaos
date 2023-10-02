3937aacec2d60ce47de9f6b3d0eaa440e8319aa8
mikleusevicmarin <mikleusevicmarin@gmail.com>
2023-10-02 14:04:41 +0200

    Adjusted connection scene flow, added logic for disconnects and full game lobbies

---
+++ Assets/Scripts/CharacterSelectReady.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CharacterSelectReady : NetworkBehaviour
+{
+    public static CharacterSelectReady Instance { get; private set; }
+
+    private Dictionary<ulong, bool> playerReadyDictonary;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        playerReadyDictonary = new Dictionary<ulong, bool>();
+    }
+
+    public void SetPlayerReady()
+    {
+        SetPlayerReadyServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SetPlayerReadyServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        playerReadyDictonary[serverRpcParams.Receive.SenderClientId] = true;
+
+        bool allClientsReady = true;
+        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+        {
+            if (!playerReadyDictonary.ContainsKey(clientId) || !playerReadyDictonary[clientId])
+            {
+                allClientsReady = false;
+                break;
+            }
+        }
+
+        if (allClientsReady)
+        {
+            Loader.LoadNetwork(Loader.Scene.GameScene);
+        }
+    }
+}
--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -10,7 +10,7 @@ public class MainMenuUI : MonoBehaviour
     {
         playButton.onClick.AddListener(() =>
         {
-            Loader.Load(Loader.Scene.GameScene);
+            Loader.Load(Loader.Scene.LobbyScene);
         });
 
         quitButton.onClick.AddListener(() =>
